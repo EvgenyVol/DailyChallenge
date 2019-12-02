206de3f2bb3bb46c3fa92c56cb0ee97c9f52c879
EvgenyVol <EvgenyB16@gmail.com>
2019-12-02 04:09:02 +0300

    Add exercise Pangram

---
+++ pangram/Pangram.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        int CodeLetterA = (int)'a';
+
+        int[] isUsedLetters = new int[26];
+
+        int i = 0;
+
+        input = input.ToLower();
+
+        while (i < input.Length)
+        {
+            if (Char.IsLetter(input[i]) && isUsedLetters[(int)input[i] - CodeLetterA] == 0)
+            {
+                isUsedLetters[(int)input[i] - CodeLetterA] = 1;
+            }
+            i++;
+        }
+
+        i = 0;
+        while (i < 26)
+        {
+            if (isUsedLetters[i] == 0)
+            {
+                return false;
+            }
+            i++;
+        }
+
+        return true;
+    }
+}
